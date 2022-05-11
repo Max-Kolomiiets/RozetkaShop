1737c46dfe6e9c67ebfe53a0ef9c880c8329ed13
Max Kolomiiets <kmaks1364@gmail.com>
2022-05-11 23:45:43 +0300

    Added models for cart group

---
+++ app/Models/Order.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['order_number', 'order_date', 'user_id', 'order_status_id'];
+
+    public function products()
+    {
+        return $this->hasMany(OrderProduct::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function orderStatus()
+    {
+        return $this->belongsTo(OrderStatus::class);
+    }
+}

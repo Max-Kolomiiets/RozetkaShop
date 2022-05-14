bb9b636a9552276b4bf491d76330bc9d8e4c9370
Max Kolomiiets <kmaks1364@gmail.com>
2022-05-14 22:29:02 +0300

    Call seeder for admin table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Encore\Admin\Auth\Database\AdminTablesSeeder;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +14,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call(AdminTablesSeeder::class);
     }
 }

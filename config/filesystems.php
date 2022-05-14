60a6a07deb824950021417a4622b5f3a7285c3f9
Max Kolomiiets <kmaks1364@gmail.com>
2022-05-14 17:20:13 +0300

    Added laravel-admin package

--- config/filesystems.php
+++ config/filesystems.php
@@ -29,7 +29,12 @@ return [
     */
 
     'disks' => [
-
+        'admin' => [
+             'driver' => 'local',
+             'root' => storage_path('app/public'),
+             'url' => env('APP_URL').'/storage',
+             'visibility' => 'public',
+             ],
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -52,7 +57,6 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
         ],
-
     ],
 
     /*
